d5eeae27d7eb050b3570efe748a371d2b12a7f00
Jesse Dearing <jesse.dearing@gmail.com>
2012-10-30 06:00:05 -0500

    Sets up generators

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,10 @@ module Oregonocalypse
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.template_engine :haml
+      g.test_framework :rspec
+    end
   end
 end
