4abc1e3a30a6e9eb77b869edb95ad909b566da6a
Jesse Dearing <jesse.dearing@gmail.com>
2012-10-29 23:13:52 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oregonocalypse::Application.config.secret_token = '109b502c25e8984a79fe45af238d0183dfd743cd8f6cb8893840e68f366b52f05daa50ede8141cfea8631c12d77d2ee2a8e6b72b9e019ae3bf33b54975cddb12'
